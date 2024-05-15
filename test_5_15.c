ef96bb762280ed41329f15d7c3a33f25b3e323b7
wangjun <1556405648@qq.com>
2024-05-15 21:40:31 +0800

    2024/5/15 数据存储做题,并且复习以前的知识点

--- test_5_15.c
+++ test_5_15.c
@@ -1,3 +1,4 @@
+#include <assert.h>
 #include <stdio.h>
 #include <string.h>
 /**
@@ -155,4 +156,130 @@
 
 
 
+//实现strcpy库函数
+// char* my_strcpy(char* dest,char* str)
+// {
+//     char* ret = dest;
+//     assert(str != NULL);
+//     assert(dest !=NULL);
+//     while (*dest++ = *str++)
+//     {
+//         ;
+//     }
+//     return ret;
+// }
+// int main()
+// {
+//     char arr1[20] = "xxxxxxxxxxxxxx";
+//     char arr2[] = "hello bit";
+//     printf("%s\n",my_strcpy(arr1,arr2));
+//     return 0;
+// }
 
+//实现strlen函数
+// int my_strlen( const char* str)
+// {
+//     assert(str);
+//     int count = 0;
+//     while (*str != '\0')
+//     {
+//         count++;
+//         str++;
+//     }
+//     return count;
+// }
+// int main()
+// {
+//     char arr[] = "abcdef";
+//     int ret = my_strlen(arr);
+//     printf("%d\n",ret);
+//     return 0;
+// }
+
+// 输入一个整数数组，实现一个函数，
+//
+// 来调整该数组中数字的顺序使得数组中所有的奇数位于数组的前半部分，
+//
+// 所有偶数位于数组的后半部分。
+//法一:这是从前和后同时进行,找到了前偶数后奇数就交换位置
+// void move_odd_even(int arr[],int sz)
+// {
+//     int left = 0;
+//     int right = sz -1;
+//     while (left < right)
+//     {
+//       //如果是奇数就跳过去
+//       while ((left < right) && (arr[left] %2 == 1)) //添加一个前提条件，防止都是偶数或者奇数的时候发生越界
+//       {
+//         left++;
+//       }
+//       //如果是偶数也跳过去，往后走
+//       while ((left < right) && (arr[right] %2 == 0))
+//       {
+//         right--;
+//       }
+//       while (left < right)
+//       {
+//         int tmp = arr[left];
+//         arr[left] = arr[right];
+//         arr[right] = tmp;
+//         left++;
+//         right--;
+//       }
+//     }
+// }
+// int main()
+// {
+//   int arr[10] ={0};
+//   int i = 0;
+//   int sz = sizeof(arr) / sizeof(arr[0]);
+//   for (i = 0; i < sz ;i++)
+//   {
+//     scanf("%d",arr+i);
+//   }
+//   //调整
+//   move_odd_even(arr,sz);
+//   //打印
+//   for (i = 0; i < sz; i++)
+//   {
+//     printf("%d ",arr[i]);
+//   }
+//   return 0;
+// }
+
+//法二:这是用冒泡实现的,并且有顺序的前奇数,后偶数
+void move_odd_even(int arr[],int n)
+{
+  int i = 0;
+  for (i = 0; i < n ;i++)
+  {
+    int j = 0;
+    for (j = 0 ;j < n - 1 -i; j++)
+    {
+      if(arr[j]%2 == 0 && arr[j+1] %2 !=0)
+      {
+        int tmp = arr[j];
+        arr[j] = arr[j+1];
+        arr[j+1] = tmp;
+      }
+    }
+  }
+}
+int main()
+{
+  int arr[10] = {0};
+  int i = 0;
+  int sz = sizeof(arr) / sizeof(arr[0]);
+  for (i = 0; i < sz; i++)
+  {
+    scanf("%d",arr + i);
+  }
+  //调整
+  move_odd_even(arr,sz);
+  //打印
+  for (i = 0; i < sz; i++)
+  {
+    printf("%d ",arr[i]);
+  }
+  return 0;
+}
