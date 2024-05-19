00ac03a48e0cf7fc572ece7eb728508cae4d6f79
wangjun <1556405648@qq.com>
2024-05-19 16:48:15 +0800

    函数指针数组和转义表

--- test_5_19.c
+++ test_5_19.c
@@ -85,70 +85,166 @@
 // }
 
 //函数指针的简单应用
-void menu()
-{
-    printf("**************************\n");
-    printf("***** 1.Add 2.Sub ********\n");
-    printf("***** 3.Mul 4.Div ********\n");
-    printf("*****     0.退出   ********\n");
-    printf("**************************\n");
-    printf("**************************\n");
-}
+// void menu()
+// {
+//     printf("**************************\n");
+//     printf("***** 1.Add 2.Sub ********\n");
+//     printf("***** 3.Mul 4.Div ********\n");
+//     printf("*****     0.退出   ********\n");
+//     printf("**************************\n");
+//     printf("**************************\n");
+// }
+// int Add(int x, int y)
+// {
+//     return x + y;
+// }
+// int Sub(int x, int y)
+// {
+//     return x - y;
+// }
+// int Mul(int x, int y)
+// {
+//     return x * y;
+// }
+// int Div(int x, int y)
+// {
+//     return x / y;
+// }
+// void cacl(int (*pf)(int , int ))
+// {
+//     int x = 0;
+//     int y = 0;
+//     int ret = 0;
+//     printf("请输入两个操作数:>");
+//     scanf("%d %d",&x,&y);
+//     ret = pf(x,y);
+//     printf("%d\n",ret);
+// }
+// int main()
+// {
+//     int input = 0;
+//     do
+//     {
+//         menu();
+//         printf("请选择:>");
+//         scanf("%d",&input);
+//         switch (input)
+//         {
+//             case 1:
+//                 cacl(Add);
+//                 break;
+//             case 2:
+//                 cacl(Sub);
+//                 break;
+//             case 3:
+//                 cacl(Mul);
+//                 break;
+//             case 4:
+//                 cacl(Div);
+//                 break;
+//             case 0:
+//                 printf("退出计算\n");
+//                 break;
+//             default:
+//                 printf("输入错误,请重新输入\n");
+//                 break;
+//         }
+//     } while (input);
+//     return 0;
+// }
+
+// //转义表
+// void menu()
+// {
+//   printf("**************************\n");
+//   printf("***** 1.Add 2.Sub ********\n");
+//   printf("***** 3.Mul 4.Div ********\n");
+//   printf("*****     0.退出   ********\n");
+//   printf("**************************\n");
+//   printf("**************************\n");
+// }
+// int Add(int x, int y)
+// {
+//   return x + y;
+// }
+// int Sub(int x, int y)
+// {
+//   return x - y;
+// }
+// int Mul(int x, int y)
+// {
+//   return x * y;
+// }
+// int Div(int x, int y)
+// {
+//   return x / y;
+// }
+// void cacl(int (*pf)(int , int ))
+// {
+//   int x = 0;
+//   int y = 0;
+//   int ret = 0;
+//   printf("请输入两个操作数:>");
+//   scanf("%d %d",&x,&y);
+//   ret = pf(x,y);
+//   printf("%d\n",ret);
+// }
+// int main()
+// {
+//   int input = 0;
+//   int (*pfarr[])(int ,int ) = {0,Add,Sub,Mul,Div};
+//   do
+//   {
+//     menu();
+//     printf("请选择:>");
+//     scanf("%d",&input);
+//     if (input == 0)
+//     {
+//       printf("退出计算\n");
+//       break;
+//     }
+//     else if(input>=1 && input <=4)
+//     {
+//       int x = 0;
+//       int y = 0;
+//       int ret = 0;
+//       printf("请输入两个操作数:>");
+//       scanf("%d %d",&x,&y);
+//       ret = pfarr[input](x,y);
+//       printf("%d\n",ret);
+//     }
+//     else
+//     {
+//       printf("输入错误请重新输入\n");
+//     }
+//
+//   } while (input);
+//   return 0;
+// }
 int Add(int x, int y)
 {
-    return x + y;
+  return x + y;
 }
 int Sub(int x, int y)
 {
-    return x - y;
+  return x - y;
 }
 int Mul(int x, int y)
 {
-    return x * y;
+  return x * y;
 }
 int Div(int x, int y)
 {
-    return x / y;
-}
-void cacl(int (*pf)(int , int ))
-{
-    int x = 0;
-    int y = 0;
-    int ret = 0;
-    printf("请输入两个操作数:>");
-    scanf("%d %d",&x,&y);
-    ret = pf(x,y);
-    printf("%d\n",ret);
+  return x / y;
 }
 int main()
 {
-    int input = 0;
-    do
+    //定义一个函数指针数组
+    int (*pfarr[])(int,int) = {Add,Sub,Mul,Div};
+    int i = 0;
+    for (i = 0; i < 4;i++ )
     {
-        menu();
-        printf("请选择:>");
-        scanf("%d",&input);
-        switch (input)
-        {
-            case 1:
-                cacl(Add);
-                break;
-            case 2:
-                cacl(Sub);
-                break;
-            case 3:
-                cacl(Mul);
-                break;
-            case 4:
-                cacl(Div);
-                break;
-            case 0:
-                printf("退出计算\n");
-                break;
-            default:
-                printf("输入错误,请重新输入\n");
-                break;
-        }
-    } while (input);
+      printf("%d ",pfarr[i](8,4));//分别调用数组中的四个函数,结果12 4 32 2
+    }
     return 0;
 }
