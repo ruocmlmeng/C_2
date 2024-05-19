5477c418fc72849043d3a3abe85e7f2211d5be54
wangjun <1556405648@qq.com>
2024-05-19 20:10:28 +0800

    将冒泡排序实现成qsort一样的功能

--- test_5_19.c
+++ test_5_19.c
@@ -302,3 +302,83 @@
 //   test();
 //   return 0;
 // }
+
+//实现冒泡排序
+struct Stu
+{
+  char name[20];
+  int age;
+};
+void Swap(char* buf1,char* buf2,int width)
+{
+  int i = 0;
+  for (i = 0; i <width ; i++)
+  {
+    char tmp = *buf1;
+    *buf1 = *buf2;
+    *buf2 = tmp;
+    buf1++;
+    buf2++;
+  }
+}
+void bubble_sort(void* base,int sz,int width,int(*cmp)(const void* e1,const void* e2))
+{
+  int i = 0;
+  for (i = 0; i < sz -1;i++)
+  {
+    int flag = 1;
+    int j = 0;
+    for (j = 0; j < sz - 1 -i;j++)
+    {
+      if(cmp((char*)base+j*width,(char*)base + (j+1)*width) > 0)
+      {
+        //交换
+        Swap((char*)base+j*width ,(char*)base + (j+1)*width ,width);
+        flag = 0;
+      }
+      if (flag == 1)
+      {
+        break;
+      }
+    }
+  }
+}
+int cmp_int(const void* e1,const void* e2)
+{
+  return (*(int*)e1 - *(int*)e2);
+}
+//按名字来排序
+int cmp_stu_by_name(const void*e1, const void* e2)
+{
+  return strcmp(((struct Stu*)e1)->name,((struct Stu*)e2)->name);
+}
+void test()
+{
+  int arr[] = {9,8,7,6,5,4,3,2,1,0};
+  int sz = sizeof(arr) / sizeof(arr[0]);
+  bubble_sort(arr,sz,sizeof(arr[0]),cmp_int);
+  int i = 0;
+  for (i = 0; i < sz ; i++)
+  {
+    printf("%d ",arr[i]);
+  }
+}
+void test1()
+{
+  struct Stu s[3] = {{"zhangsan",15},{"lisi",30},{"wangwu",25}};
+  int sz = sizeof(s) / sizeof(s[0]);
+  // qsort(s,sz,sizeof(s[0]),cmp_stu_by_name);
+  bubble_sort(s,sz,sizeof(s[0]),cmp_stu_by_name);
+  int i = 0;
+  for (i = 0; i < sz ;i++)
+  {
+    printf("%s %d\n",s[i].name,s[i].age);
+
+  }
+}
+int main()
+{
+  // test();
+  test1();
+  return 0;
+}
