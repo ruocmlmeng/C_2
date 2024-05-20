2d1578b24efdde70f876496bf4858a95c50944db
wangjun <1556405648@qq.com>
2024-05-20 21:27:21 +0800

    指针笔试题

--- test_5_20.c
+++ test_5_20.c
@@ -76,23 +76,115 @@
 //   return 0;
 // }
 
+// int main()
+// {
+//   char* p ="abcdef"; //*arr -> arr[0]
+//   // printf("1 %d\n",sizeof(p));//7-->8 p存的是一个地址
+//   // printf("2 %d\n",sizeof(p + 1));//随机值 ->8 地址+1还是地址
+//   // printf("3 %d\n",sizeof(*p));//1
+//   // printf("4 %d\n",sizeof(p[0]));//1
+//   // printf("5 %d\n",sizeof(&p));//4/8 取一级指针的地址->二级指针
+//   // printf("6 %d\n",sizeof(&p + 1));//4/8
+//   // printf("7 %d\n",sizeof(&p[0] + 1));//4/8
+//
+//   printf("1 %d\n",strlen(p));//6
+//   printf("2 %d\n",strlen(p + 1));//随机值 ->5
+//   // printf("3 %d\n",strlen(*p));//err
+//   // printf("4 %d\n",strlen(p[0]));//err
+//   printf("5 %d\n",strlen(&p));//6 ->随机值
+//   printf("6 %d\n",strlen(&p + 1));//随机值
+//   printf("7 %d\n",strlen(&p[0] + 1));//5
+//   return 0;
+// }
+
+//---------指针笔试题---------
+//1.
+// int main()
+// {
+//   int a[5] = {1,2,3,4,5};
+//   int* ptr = (int*)(&a + 1);
+//   printf("%d,%d",*(a+1),*(ptr - 1));//2 5
+//   return 0;
+// }
+//2.
+// struct Test
+// {
+//   int Num;
+//   char* pcName;
+//   short sDate;
+//   char cha[2];
+//   short sBa[4];
+// }*p = (struct Test*)0x100000;
+// //假设p 的值为0x100000.如下表表达式的值分别为多少?
+// //已知,结构体Test类型的变量大小是20个字节
+// int main()
+// {
+//   printf("%p\n",p + 0x1);//0000000000100020
+//   //跳过一个结构体指针的大小
+//   //0x100000+20 --> 0x100014
+//   printf("%p\n",(unsigned long)p + 0x1);//0000000000100001
+//   //整形+1
+//   //0x100001
+//   printf("%p\n",(unsigned int*)p + 0x1);//0000000000100004
+//   //跳过一个整形指针的大小
+//   //0x100000+4->0x100004
+//   return 0;
+// }
+//3.小端存储
+// int main()
+// {
+//   int a[4] = {1,2,3,4};
+//   int* ptr1 = (int* )(&a+1);
+//   int* ptr2 = (int* )((int)a + 1);
+//   printf("%x,%x",ptr1[-1],*ptr2);//4 ,20000000
+//   return 0;
+// }
+// int main()
+// {
+//   int a[3][2] = {(0,1),(2,3),(4,5)};//考查了逗号表达式
+//   int* p;
+//   p = a[0];
+//   printf("%d",p[0]);//a[0]表示的就是首元素地址,a[0][0]
+//   return 0;
+// }
+//4.---难题
+// int main()
+// {
+//   int a[5][5];
+//   int(*p)[4];
+//   p = a;
+//   printf("%p,%d\n",&p[4][2] - &a[4][2],&p[4][2] - &a[4][2]);//FFFFFFFFFFFFFFFC,-4
+//   return 0;
+// }
+//5.自己能分析出来的题目
+// int main()
+// {
+//   int aa[2][5] = {1,2,3,4,5,6,7,8,9,10};
+//   int* ptr1 = (int*)(&aa + 1);
+//   int* ptr2 = (int*)(*(aa+1));
+//   printf("%d,%d",*(ptr1 - 1),*(ptr2 - 1));//10 5
+//   return 0;
+// }
+
+//6.
+// int main()
+// {
+//   char *a[] = {"work","at","alibaba"};
+//   char**pa = a;
+//   pa++;
+//   printf("%s\n",*pa); //at
+//   return 0;
+// }
+
+//7.该题要画图解出来,看指针在内存中的指向-----很好的一道题
 int main()
 {
-  char* p ="abcdef"; //*arr -> arr[0]
-  // printf("1 %d\n",sizeof(p));//7-->8 p存的是一个地址
-  // printf("2 %d\n",sizeof(p + 1));//随机值 ->8 地址+1还是地址
-  // printf("3 %d\n",sizeof(*p));//1
-  // printf("4 %d\n",sizeof(p[0]));//1
-  // printf("5 %d\n",sizeof(&p));//4/8 取一级指针的地址->二级指针
-  // printf("6 %d\n",sizeof(&p + 1));//4/8
-  // printf("7 %d\n",sizeof(&p[0] + 1));//4/8
-
-  printf("1 %d\n",strlen(p));//6
-  printf("2 %d\n",strlen(p + 1));//随机值 ->5
-  // printf("3 %d\n",strlen(*p));//err
-  // printf("4 %d\n",strlen(p[0]));//err
-  printf("5 %d\n",strlen(&p));//6 ->随机值
-  printf("6 %d\n",strlen(&p + 1));//随机值
-  printf("7 %d\n",strlen(&p[0] + 1));//5
+  char* c[] = {"ENTER","NEW","POINT","FIRST"};
+  char** cp[] = {c + 3,c + 2,c + 1,c};
+  char*** cpp = cp;
+  printf("%s\n",**++cpp);
+  printf("%s\n",*-- * ++cpp +3);
+  printf("%s\n",*cpp[-2] + 3);
+  printf("%s\n",cpp[-1][-1] + 1);
   return 0;
 }
